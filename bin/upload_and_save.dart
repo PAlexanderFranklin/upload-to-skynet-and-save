8ef87d5d61c53caec7b6dd1290d0927acb44ecb9
PAlexanderFranklin <53384571+RoaringDragon2@users.noreply.github.com>
2022-06-03 22:46:41 -0700

    Add runtime config

--- bin/upload_and_save.dart
+++ bin/upload_and_save.dart
@@ -1,12 +1,33 @@
 import 'dart:io';
+import 'dart:convert';
 
 // import 'package:upload_and_save/upload_and_save.dart' as upload_and_save;
 
 void main(List<String> arguments) async {
-  var source = Directory('source');
+  final configJson = await File('upload_and_save_config.json')
+      .readAsString()
+      .then((String contents) {
+    return jsonDecode(contents);
+  });
+
+  final skynetRegistrySeed = configJson["skynetRegistrySeed"];
+  final source = configJson["source"];
+  final destination = configJson["destination"];
+
+  if (!(skynetRegistrySeed is String &&
+      source is String &&
+      destination is String)) {
+    print("""Useable config file not found. Ensure you have a file named
+      "upload_and_save_config.json" that contains a skynetRegistrySeed,
+      source, and destination. Press enter to close.""");
+    stdin.readLineSync();
+    return null;
+  }
+
+  var sourceDirectory = Directory(source);
 
   try {
-    var sourceList = source.list();
+    var sourceList = sourceDirectory.list();
     await for (final FileSystemEntity f in sourceList) {
       if (f is File) {
         print('Found file ${f.path}');

614a1fe2a5fc9c349166600821012a8fda887f4e
PAlexanderFranklin <53384571+RoaringDragon2@users.noreply.github.com>
2022-06-03 21:43:11 -0700

    Add directory code, ignore exe file

--- bin/upload_and_save.dart
+++ bin/upload_and_save.dart
@@ -1,5 +1,23 @@
-import 'package:upload_and_save/upload_and_save.dart' as upload_and_save;
+import 'dart:io';
 
-void main(List<String> arguments) {
-  print('Hello world: ${upload_and_save.calculate()}!');
+// import 'package:upload_and_save/upload_and_save.dart' as upload_and_save;
+
+void main(List<String> arguments) async {
+  var source = Directory('source');
+
+  try {
+    var sourceList = source.list();
+    await for (final FileSystemEntity f in sourceList) {
+      if (f is File) {
+        print('Found file ${f.path}');
+      } else if (f is Directory) {
+        print('Found dir ${f.path}');
+      }
+    }
+  } catch (e) {
+    print(e.toString());
+  }
+  print('press enter to close.');
+  stdin.readLineSync();
+  // print('Hello world: ${upload_and_save.calculate()}!');
 }

9443c07382ba9c502307f7bcfbcfe059ddb7c2b0
PAlexanderFranklin <53384571+RoaringDragon2@users.noreply.github.com>
2022-08-04 00:50:10 -0700

    get sqlite working

--- README.md
+++ README.md
@@ -1,1 +1,4 @@
 # Upload to skynet and save with sqlite
+
+## Setup
+Put the compiled executable, database, config json, and sqlite3.dll all in the same folder. Configure the json file.
--- bin/upload_and_save.dart
+++ bin/upload_and_save.dart
@@ -1,8 +1,10 @@
 import 'dart:io';
 import 'dart:convert';
 
-import 'package:skynet/skynet.dart';
-import 'package:upload_and_save/upload_and_save.dart' as upload_and_save;
+import 'package:skynet/skynet.dart' as skynet;
+import 'package:sqlite3/sqlite3.dart';
+import 'package:path/path.dart' as path;
+// import 'package:upload_and_save/upload_and_save.dart' as upload_and_save;
 
 void main(List<String> arguments) async {
   final configJson = await File('upload_and_save_config.json')
@@ -15,7 +17,10 @@ void main(List<String> arguments) async {
   final source = configJson["source"];
   final destination = configJson["destination"];
   final skynetAPIKey = configJson["skynetAPIKey"];
-  final skynetPortal = configJson["skynetPortal"];
+  final skynetPortal = configJson["skynetPortal"] ?? 'siasky.net';
+  final databaseName = configJson["databaseName"] ?? 'MetaData.db';
+
+  final skynetClient = skynet.SkynetClient(portal: skynetPortal);
 
   if (!(skynetRegistrySeed is String &&
       source is String &&
@@ -30,21 +35,34 @@ void main(List<String> arguments) async {
   var sourceDirectory = Directory(source);
 
   try {
+    Database db = sqlite3.open(databaseName);
     var sourceList = sourceDirectory.list();
     await for (final FileSystemEntity f in sourceList) {
       if (f is File) {
         print('Found file ${f.path}');
+        String fileNameWithoutExtension = path.basenameWithoutExtension(f.path);
+
         // Upload
+        // skynetClient.upload.uploadFile(f);
+        String skylink = "sia://123123";
+
         // Save Skylink
+        db.execute("""
+          UPDATE audio
+          SET skylink = $skylink
+          WHERE file_name = $fileNameWithoutExtension;
+        """);
+
         // Move to destination
       }
     }
     // Upload new DB
+    db.dispose();
     // Save Skylink To Log File
   } catch (e) {
     print(e.toString());
   }
-  print('Hello world: ${upload_and_save.calculate()}!');
+  // print('Hello world: ${upload_and_save.calculate()}!');
   print('press enter to close.');
   stdin.readLineSync();
 }
